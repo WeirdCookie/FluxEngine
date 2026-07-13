3376cd9f4e484d39e14f771022052a4396730747
WeirdCookie <olebeuscher@gmail.com>
2026-07-13 20:17:58 +0200

    Implement run method for Flow struct

--- Backend/flow.rs
+++ Backend/flow.rs
@@ -3,3 +3,12 @@ use crate::node::Node;
 pub struct Flow {
     pub nodes: Vec<Node>,
 }
+
+impl Flow {
+    pub fn run(&self) {
+        println!("Flow hat {} Node(s).", self.nodes.len());
+        for node in &self.nodes {
+            node.run();
+        }
+    }
+}
